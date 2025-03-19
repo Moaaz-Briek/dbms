5e70dc869dcdfc161f2efb7de43b8a6d2dcdeb3f
Moaaz <moaaz.briek97@gmail.com>
2025-03-19 02:58:42 +0200

    feat: list databases inside data dir

--- handlers/database_handler.sh
+++ handlers/database_handler.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+list_databases() {
+    show_header "List Databases"
+
+    if [ -d "$DATA_DIR" ]; then
+        db_count=$(ls -l "$DATA_DIR" | grep ^d | wc -l)
+
+        if [ $db_count -eq 0 ]; then
+            echo "No databases found."
+        else
+            echo -e "${YELLOW}Available Databases:${NC}"
+            ls -l "$DATA_DIR" | grep ^d | awk '{print $9}'
+        fi
+    else
+        echo "Data directory does not exist."
+    fi
+
+    echo ""
+    echo "Press Enter to continue..."
+    read
+}
+
+create_database() {
+    echo ""
+}
+
+connect_to_database() {
+    echo ""
+}
+
+drop_database() {
+    echo ""
+}
